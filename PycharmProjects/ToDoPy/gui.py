14b696e8c3575a1c5f510be5381879969439c49e
Michał Krysiński <michalkrysinski138@gmail.com>
2025-02-20 02:15:13 +0100

    update models.py and gui.py files

--- PycharmProjects/ToDoPy/gui.py
+++ PycharmProjects/ToDoPy/gui.py
@@ -1,18 +1,22 @@
 import tkinter as tk
 from tkinter import messagebox
-from models import create_task, get_tasks, update_task, delete_task
+from models import create_task, get_tasks, update_task, delete_task_from_db, get_task_name, update_task_name_in_db
 import time
+
 
 def update_time():
     current_time = time.strftime('%A, %Y-%m-%d %H:%M')
     time_label.config(text=current_time)
     time_label.after(60000, update_time)
 
+
 def run_gui():
     global task_entry, task_frame, time_label
 
+
     root = tk.Tk()
     root.title("ToDoPy - Task Manager")
+
 
     canvas = tk.Canvas(root)
     scrollbar = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
@@ -30,16 +34,20 @@ def run_gui():
         lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
     )
 
+
     time_label = tk.Label(root, font=('Arial', 12), fg='black', anchor="w")
     time_label.pack(side="left", padx=10, pady=10, anchor="sw")
 
     update_time()
     display_tasks()
 
-    add_button = tk.Button(root, text="Add", command=add_task_window, bg='white', fg='#42a5f5', font=('Arial', 12, 'bold'))
+
+    add_button = tk.Button(root, text="dodaj", command=add_task_window, bg='white', fg='#42a5f5', font=('Arial', 12, 'bold'))
     add_button.pack(pady=10, side="bottom", anchor="w")
 
+
     root.mainloop()
+
 
 def display_tasks():
     for widget in task_frame.winfo_children():
@@ -51,7 +59,9 @@ def display_tasks():
         task_name = task[1]
         completed = task[2]
 
+
         task_color = 'green' if completed else 'black'
+
 
         task_label = tk.Label(task_frame, text=f"{index + 1}. ", width=5, font=('Arial', 14, 'bold'), bg='#f0f0f0', anchor='w', fg=task_color)
         task_name_label = tk.Label(task_frame, text=task_name, width=50, font=('Arial', 14), bg='#f0f0f0', anchor='w', fg=task_color, wraplength=350)
@@ -59,22 +69,24 @@ def display_tasks():
         task_label.grid(row=index, column=0, padx=10, pady=5, sticky='w')
         task_name_label.grid(row=index, column=1, padx=10, pady=5, sticky='w')
 
+
         options_frame = tk.Frame(task_frame, bg='#f0f0f0')
 
-        edit_button = tk.Button(options_frame, text="Edit", command=lambda t_id=task_id: edit_task(t_id), bg='#6A4C9C', fg='white', font=('Arial', 12, 'bold'))
+        edit_button = tk.Button(options_frame, text="edytuj", command=lambda t_id=task_id: edit_task(t_id), bg='#6A4C9C', fg='white', font=('Arial', 12, 'bold'))
         edit_button.grid(row=0, column=0, padx=5, pady=5)
 
-        delete_button = tk.Button(options_frame, text="Delete", command=lambda t_id=task_id: delete_task(t_id), bg='#6A4C9C', fg='white', font=('Arial', 12, 'bold'))
+        delete_button = tk.Button(options_frame, text="usuń", command=lambda t_id=task_id: delete_task(t_id), bg='#6A4C9C', fg='white', font=('Arial', 12, 'bold'))
         delete_button.grid(row=0, column=1, padx=5, pady=5)
 
         if completed == 0:
-            mark_button = tk.Button(options_frame, text="Mark as Completed", command=lambda t_id=task_id: mark_task_as_completed(t_id), bg='#6A4C9C', fg='white', font=('Arial', 12, 'bold'))
+            mark_button = tk.Button(options_frame, text="gotowe", command=lambda t_id=task_id: mark_task_as_completed(t_id), bg='#6A4C9C', fg='white', font=('Arial', 12, 'bold'))
             mark_button.grid(row=0, column=2, padx=5, pady=5)
         else:
-            unmark_button = tk.Button(options_frame, text="Unmark as Completed", command=lambda t_id=task_id: unmark_task_as_completed(t_id), bg='#6A4C9C', fg='white', font=('Arial', 12, 'bold'))
+            unmark_button = tk.Button(options_frame, text="anuluj", command=lambda t_id=task_id: unmark_task_as_completed(t_id), bg='#6A4C9C', fg='white', font=('Arial', 12, 'bold'))
             unmark_button.grid(row=0, column=2, padx=5, pady=5)
 
         options_frame.grid(row=index, column=2, padx=5, pady=5, sticky='w')
+
 
 def edit_task(task_id):
     task_name = get_task_name(task_id)
@@ -101,39 +113,21 @@ def edit_task(task_id):
     submit_button = tk.Button(edit_window, text="Save", command=update_task_name, bg='white', fg='#42a5f5', font=('Arial', 12, 'bold'))
     submit_button.pack(pady=10)
 
-def get_task_name(task_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT task_name FROM tasks WHERE id = ?", (task_id,))
-    task_name = cursor.fetchone()[0]
-    conn.close()
-    return task_name
-
-def update_task_name_in_db(task_id, new_name):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("UPDATE tasks SET task_name = ? WHERE id = ?", (new_name, task_id))
-    conn.commit()
-    conn.close()
 
 def delete_task(task_id):
     delete_task_from_db(task_id)
     display_tasks()
 
-def delete_task_from_db(task_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
-    conn.commit()
-    conn.close()
 
 def mark_task_as_completed(task_id):
     update_task(task_id, 1)
     display_tasks()
 
+
 def unmark_task_as_completed(task_id):
     update_task(task_id, 0)
     display_tasks()
+
 
 def add_task_window():
     add_window = tk.Toplevel()
--- PycharmProjects/ToDoPy/models.py
+++ PycharmProjects/ToDoPy/models.py
@@ -9,35 +9,54 @@ def connect_db():
         return None
 
 def create_task(task_name):
-    try:
-        conn = connect_db()
+    conn = connect_db()
+    if conn is not None:
         cursor = conn.cursor()
         cursor.execute("INSERT INTO tasks (task_name, completed) VALUES (?, ?)", (task_name, 0))
         conn.commit()
-        print(f"Task '{task_name}' has been added to the database.")
-    except sqlite3.OperationalError as e:
-        print("Błąd bazy danych:", e)
-    finally:
         conn.close()
 
 def get_tasks():
     conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, task_name, completed FROM tasks")
-    tasks = cursor.fetchall()
-    conn.close()
-    return tasks
+    if conn is not None:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, task_name, completed FROM tasks")
+        tasks = cursor.fetchall()
+        conn.close()
+        return tasks
+    return []
 
 def update_task(id, completed):
     conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("UPDATE tasks SET completed = ? WHERE id = ?", (completed, id))
-    conn.commit()
-    conn.close()
+    if conn is not None:
+        cursor = conn.cursor()
+        cursor.execute("UPDATE tasks SET completed = ? WHERE id = ?", (completed, id))
+        conn.commit()
+        conn.close()
 
-def delete_task(task_id):
+def delete_task_from_db(task_id):
     conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
-    conn.commit()
-    conn.close()
+    if conn is not None:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
+        conn.commit()
+        conn.close()
+
+def get_task_name(task_id):
+    conn = connect_db()
+    if conn is not None:
+        cursor = conn.cursor()
+        cursor.execute("SELECT task_name FROM tasks WHERE id = ?", (task_id,))
+        task_name = cursor.fetchone()
+        conn.close()
+        if task_name:
+            return task_name[0]
+    return None
+
+def update_task_name_in_db(task_id, new_name):
+    conn = connect_db()
+    if conn is not None:
+        cursor = conn.cursor()
+        cursor.execute("UPDATE tasks SET task_name = ? WHERE id = ?", (new_name, task_id))
+        conn.commit()
+        conn.close()
